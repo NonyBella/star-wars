62af8c33d3033815bb061c8e85311233ce8d453b
Nony Bella <miriam.odenyi@gmail.com>
2023-04-20 19:22:40 +0100

    updated file

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 export default function MovieCard(props) {
   return (
-    <div className="movie-card">
-      {props.title}
-      <br />
-      {props.date}
-      <br />
-      {props.description}
+    
+    <div className='movieContainer'>
+      <div className="movie-card">
+        {props.title}
+        <br />
+        {props.date}
+        <br />
+        {props.description}
+      </div>
     </div>
+  
   );
 }
