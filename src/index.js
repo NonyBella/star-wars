ffccde79bfe809e442b06113d5bb9be0946b629a
NonyBella <99178090+NonyBella@users.noreply.github.com>
2023-05-09 12:20:22 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './App';
-import { createBrowserRouter } from 'react-router-dom';
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
